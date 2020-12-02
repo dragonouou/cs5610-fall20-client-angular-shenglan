8ef136a5b81c6c1c689a26dabea1ba948df7082e
dragonouou <shenglanouou@gmail.com>
2020-12-02 09:30:20 -0800

    changed to remote server

--- src/services/question.service.client.ts
+++ src/services/question.service.client.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-const Url = 'http://localhost:3000/api/quizzes';
+const Url = 'https://cs5610-server-node-shenglan.herokuapp.com/api/quizzes';
 
 @Injectable()
 export class QuestionServiceClient {
